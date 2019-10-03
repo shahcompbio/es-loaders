a638a9095a370d13af54cfe752e1edc6414c4dbe
redpanda-cat <leungs1@mskcc.org>
2019-10-03 11:57:20 -0400

    Add data scraper

--- mira_bulk_loader.py
+++ mira_bulk_loader.py
@@ -1,6 +1,8 @@
 from mira_loader import load_analysis
 from mira.mira_metadata_parser import all_samples
 
+from common.scrna_parser import scRNAParser
+
 import sys
 
 
@@ -10,6 +12,13 @@ def load_samples(filepath, host="localhost", port=9200):
     for sample_id, sample_record in samples.items():
         load_analysis(
             filepath, sample_id, "sample", host=host, port=port)
+
+
+def scrape_samples(filepath):
+    samples = all_samples()
+    for sample_id, sample_record in samples.items()
+    data = scRNAParser(filepath + sample_id)
+    print(data.data.colData.keys())
 
 
 if __name__ == '__main__':
