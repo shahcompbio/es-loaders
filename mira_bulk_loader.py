34b4a8a654b69de0259d4d2893184f2c93936fdf
redpanda-cat <leungs1@mskcc.org>
2019-10-09 11:44:32 -0400

    Add load new samples method

--- mira_bulk_loader.py
+++ mira_bulk_loader.py
@@ -1,31 +1,48 @@
 from mira_loader import load_analysis
-from mira.mira_metadata_parser import patient_samples
+from mira.mira_metadata_parser import patient_samples, all_samples
+
+from elasticsearch import Elasticsearch
 
 from common.scrna_parser import scRNAParser
+from mira_cleaner import clean_analysis
 
 import sys
-
-BROKEN_SAMPLE_IDS = ["SPECTRUM-OV-003_S1_CD45N_RIGHT_ADNEXA",
-                     "SPECTRUM-OV-003_S2_CD45N_RIGHT_ADNEXA",
-                     "SPECTRUM-OV-026_S1_CD45N_BOWEL",
-                     "SPECTRUM-OV-031_S1_CD45N_LYMPH_NODE",
-                     "SPECTRUM-OV-036_S1_CD45P_RIGHT_ADNEXA",
-                     "SPECTRUM-OV-003_S2_CD45P_PERITONEUM",
-                     "SPECTRUM-OV-009_S1_CD45P_RIGHT_UPPER_QUADRANT",
-                     "SPECTRUM-OV-022_S1_CD45N_RIGHT_ADNEXA",
-                     "SPECTRUM-OV-007_S1_CD45N_BOWEL",
-                     "SPECTRUM-OV-026_S1_CD45P_ASCITES"]
 
 
 def load_samples(filepath, patient_id, host="localhost", port=9200):
 
     samples = [sample_record["nick_unique_id"]
                for sample_record in patient_samples(patient_id)]
-    print
     for sample_id in samples:
-        if sample_id not in BROKEN_SAMPLE_IDS:
-            load_analysis(
-                filepath, sample_id, "sample", host=host, port=port)
+        load_analysis(
+            filepath, sample_id, "sample", host=host, port=port)
+
+
+def load_new_samples(filepath, host="localhost", port=9200):
+    all_sample_ids = [sample_id
+                      for sample_id, fields in all_samples().items()]
+
+    loaded_samples = get_loaded_samples(host=host, port=port)
+
+    for sample_id in all_sample_ids:
+        if sample_id in loaded_samples:
+            pass
+        else:
+            try:
+                load_analysis(
+                    filepath, sample_id, "sample", host=host, port=port)
+            except:
+                e = sys.exc_info()[0]
+                print(e)
+                clean_analysis("sample", sample_id, host=host, port=port)
+
+
+def get_loaded_samples(host, port):
+    es = Elasticsearch(hosts=[{'host': host, 'port': port}])
+
+    result = es.search(index="dashboard_entry", body={"size": 1000})
+
+    return [record["_source"]["dashboard_id"] for record in result["hits"]["hits"]]
 
 
 def scrape_samples(filepath, patient_id):
@@ -48,4 +65,5 @@ def scrape_samples(filepath, patient_id):
 
 
 if __name__ == '__main__':
-    load_samples(sys.argv[1], sys.argv[4], host=sys.argv[2], port=sys.argv[3])
+    load_new_samples(sys.argv[1],
+                     host=sys.argv[2], port=sys.argv[3])
