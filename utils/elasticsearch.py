598d3d1b847999fc890fab08ed8758087aae3791
redpanda-cat <leungs1@mskcc.org>
2019-12-02 15:44:13 -0500

    Change chunk size

--- utils/elasticsearch.py
+++ utils/elasticsearch.py
@@ -83,6 +83,9 @@ class ElasticsearchClient():
     def load_bulk_parallel(self, index, generator):
 
         for success, info in helpers.parallel_bulk(self.es, generator,
-                                                   index=index, doc_type="_doc"):
+                                                   index=index, doc_type="_doc",
+                                                   chunk_size=100):
+            if success:
+                print("Done", info)
             if not success:
                 print('Doc failed', info)
